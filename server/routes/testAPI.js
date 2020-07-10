19086c49eb0c725625a41c03c69ab6f4d3ed8ed1
randomguy6 <32748027+randomguy6@users.noreply.github.com>
2020-07-10 15:10:46 -0600

    Return a couple of movies from backend

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -1,8 +1,23 @@
 var express = require("express");
 var router = express.Router();
+var dag = require("../constants/DAC.json");
+var san = require("../constants/Sanju.json");
+var wm = require("../constants/WickerMan.json");
+const movies = [dag, san, wm];
+
+const stringifyMovies = function(movies){
+    ret = [];
+    movies.forEach((movie) => {
+        ret.push(JSON.stringify(movie));
+    });
+
+    return ret;
+}
 
 router.get("/", function(req, res, next){
-    res.send("API is working nicely");
+    const moviesString = stringifyMovies(movies);
+    // res.json(wm);
+    res.send(moviesString);
 });
 
 module.exports = router;
