23f4e2ef2d91f8a03379782779f897a72c69694f
randomguy6 <32748027+randomguy6@users.noreply.github.com>
2020-07-14 13:45:33 -0600

    backend handles review requests

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var cors = require("cors");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require("./routes/testAPI");
-var reviewRouter = require("./routes/review");
+var handleReview = require("./routes/review");
 
 var app = express();
 
@@ -25,7 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
-app.use("/review/:id", reviewRouter);
+// app.use("/review/:id", reviewRouter);
+app.get("/review/:id", function(req, res, next){
+  handleReview(req, res, next);
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- server/routes/review.js
+++ server/routes/review.js
@@ -1,11 +1,26 @@
 var express = require("express");
 var router = express.Router();
-// var dag = require("../constants/DAC.json");
-// var san = require("../constants/Sanju.json");
-// var wm = require("../constants/WickerMan.json");
+var dag = require("../constants/DAC.json");
+var san = require("../constants/Sanju.json");
+var wm = require("../constants/WickerMan.json");
 
-router.get("/", function(req, res, next){
-    res.send("Test run good");
-});
+const handleReview = function(req, res, next){
+    let id = req.params.id;
+    let ret = ""
+    switch(id){
+        case "The Wicker Man":
+            ret = wm.review;
+            break;
+        case "Dragged Across Concrete":
+            ret = dag.review;
+            break;
+        case "Sanju":
+            ret = san.review;
+            break;
+        default:
+            ret = "Not found"
+    }
+    res.send("Testing try 2 w/ " + ret);
+};
 
-module.exports = router;
+module.exports = handleReview;
